52e234e3090a488f3e9be9227c6ecf8dc1a325d1
David Carrillo Jr <djcarrillo6@yahoo.com>
2020-09-11 16:53:06 -0700

    mods

--- Pattern_Solving_Algos/multiplePointers.js
+++ Pattern_Solving_Algos/multiplePointers.js
@@ -9,13 +9,13 @@ Strategy Notes:
 
 - Set 2 variables to as pointers: i starts at the 0 index & j starts at the 1 index so we can now compare arr[i] to arr[j]. 
 - Through the loop, we can built up the from the begining with only the unique values.
-- After the loop has finished, all the uniques are built from 0 idx to 'j' and 'i' is now set to the idx of the last unique value, now we can return i + 1 to return the amount of unique values in te.
+- After the loop has finished, all the uniques are built from 0 idx to 'j' and 'i' is now set to the idx of the last unique value, now we can return i + 1 to return the amount of unique values.
 */
 
 
 function countUniqueValues(arr) {
 
-    // edge case for an empty []
+    // edge case for an empty array
     if (arr.length === 0) return 0;
 
     var i = 0;
--- Pattern_Solving_Algos/validAnagram.js
+++ Pattern_Solving_Algos/validAnagram.js
@@ -2,7 +2,7 @@
 Given two strings, write a function to determine if the second string is an anagram of the first.
 
 validAnagram('aaz', 'zza') // true
-validAnagram('aaz', 'zzay') // false
+validAnagram('aaz', 'zzay') // false FALSE
 */
 
 function validAnagram(first, second) {
