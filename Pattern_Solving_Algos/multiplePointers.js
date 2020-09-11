f2b055c5f39d2eca782dc66783edb05e4ef6e281
David Carrillo Jr <djcarrillo6@yahoo.com>
2020-09-10 19:37:07 -0700

    mods

--- Pattern_Solving_Algos/multiplePointers.js
+++ Pattern_Solving_Algos/multiplePointers.js
@@ -2,14 +2,14 @@
 Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array.  There can be negative numbers in the array, but it will always be sorted!
 
 countUniqueValues([1,1,1,1,2]) // 2
-countUniqueValues([1,2,3,4,4,4]) // 4
+countUniqueValues([1,2,3,4,4,4]) // 4 arr
 
 
 Strategy Notes:
 
 - Set 2 variables to as pointers: i starts at the 0 index & j starts at the 1 index so we can now compare arr[i] to arr[j]. 
 - Through the loop, we can built up the from the begining with only the unique values.
-- After the loop has finished, all the uniques are built from 0 idx to 'j' and 'i' is now set to the idx of the last unique value, now we can return i + 1 to return the amount of unique values.
+- After the loop has finished, all the uniques are built from 0 idx to 'j' and 'i' is now set to the idx of the last unique value, now we can return i + 1 to return the amount of unique values in te.
 */
 
 
