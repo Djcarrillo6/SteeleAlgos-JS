fa554bacee2fbc54d7653330298c9302025ac286
David Carrillo Jr <djcarrillo6@yahoo.com>
2020-09-15 12:57:17 -0700

    mods

--- Pattern_Solving_Algos/validAnagram.js
+++ Pattern_Solving_Algos/validAnagram.js
@@ -1,8 +1,8 @@
 /*
 Given two strings, write a function to determine if the second string is an anagram of the first.
 
-validAnagram('aaz', 'zza') // TRUE
-validAnagram('aaz', 'zzay') // FALSE 
+validAnagram('aaz', 'zza') // true
+validAnagram('aaz', 'zzay') // false 
 */
 
 function validAnagram(first, second) {
