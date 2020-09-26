bae68d60a51ccc6719028184dc7cc5df9dd60b5a
David Carrillo Jr <djcarrillo6@yahoo.com>
2020-09-25 19:21:14 -0700

    mods

--- Sorting_Algorithms/bubbleSort.js
+++ Sorting_Algorithms/bubbleSort.js
@@ -8,7 +8,7 @@ Write a function called 'bubbleSort' that accepts an array, and returns a sorted
 Psuedo-Code:
 - Start an outter loop from the end of the array to the start.
 - Start an inner loop from the start of the array until (i - 1).
-- If arr[j] is greater than arr[j + 1], swap those two values!
+- If arr[j] is greater than arr[j + 1], then swap those two values!
 - Return the sorted array.
 */
 
